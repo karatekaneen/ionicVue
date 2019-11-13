dfcc7cec478491e8a1f826da0f4864902cc51c7a
Karatekaneen <bauhn_@hotmail.com>
2019-11-13 11:36:10 +0100

    Fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import { IonicVueRouter } from '@ionic/vue'
 
-Vue.use(VueRouter)
+Vue.use(IonicVueRouter)
 
 const routes = [
 	{
@@ -16,12 +16,13 @@ const routes = [
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 	}
 ]
 
-const router = new VueRouter({
+const router = new IonicVueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
 	routes
 })
 
