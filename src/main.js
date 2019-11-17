a6b771d6caa98a5d60fe8404650916c90f015a61
karatekaneen <bauhn_@hotmail.com>
2019-11-17 23:51:21 +0100

    Fixed icons to work with vuetify etc

--- src/main.js
+++ src/main.js
@@ -3,11 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import { defineCustomElements as ionic } from '@ionic/core/loader' // added line
+// import { defineCustomElements as svgmorphing } from 'stencil-svgpaths-morphing'
+
 import Ionic from '@ionic/vue'
+// import { addIcons } from 'ionicons' // added line
+// import { ICON_PATHS } from 'ionicons/icons' // added line
 import '@ionic/core/css/ionic.bundle.css'
 import vuetify from './plugins/vuetify'
 
+Vue.config.ignoredElements = [/^ion-/, /^jeep-/] // added line
+
 Vue.use(Ionic)
+// ionic(window) // added line
+// svgmorphing(window)
+// addIcons(ICON_PATHS) // added line
 Vue.config.productionTip = false
 
 new Vue({
