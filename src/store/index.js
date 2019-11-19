92025ba4ca5c1a9c59a70b05a954b522fb7d2d6c
karatekaneen <bauhn_@hotmail.com>
2019-11-20 00:13:30 +0100

    Added commit component

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
+	state: {
+		repos: [],
+		error: null,
+		hasFetched: false
+	},
+	mutations: {
+		setRepos(state, repos) {
+			if (repos) state.repos = repos
+			else state.repos = []
+		}
+	},
+	actions: {
+		/**
+		 * Fetches repos for a specific user.
+		 * @async
+		 * @param {String} username The user whose repos we should fetch
+		 * @returns {Array<Object>|null} Returns null if user is not found, or array of repos if user has any.
+		 */
+		async getRepos({ commit, state }, username = 'karatekaneen') {
+			try {
+				// Hard coded for now, easy to extend later
+				const { data } = await axios.post('https://www.graphqlhub.com/graphql', {
+					// GraphQL Query:
+					query: `{
+						github {
+							user(username: "${username}") {
+								avatar_url
+								repos{
+									name
+									commits(limit: 5){
+										date
+										message
+									}
+								}
+							}
+						}
+					}`
+				})
+				const user = data.data.github.user // This is null if user is not found but that case is handled in the mutation.
+				commit('setRepos', user.repos)
+			} catch (err) {
+				state.error = err
+				console.error(err)
+			}
+		}
+	},
 	modules: {}
 })
--- src/views/CommitView.vue
+++ src/views/CommitView.vue
@@ -1,37 +1,52 @@
 <template>
-	<div class="home">Det här ska vara commit-lista</div>
+	<v-container style="overflow: scroll !important;">
+		<v-card style="padding: 20px; margin: 20px; " v-for="(commit, index) in commits" :key="index">
+			<v-card-title>
+				<v-icon x-large :color="index % 2 === 0 ? 'primary' : 'secondary'">{{getIcon(index)}}</v-icon>
+				{{commit.repoName}}
+			</v-card-title>
+			<v-card-subtitle>{{commit.date.toLocaleDateString()}} - {{commit.date.toLocaleTimeString() }}</v-card-subtitle>
+			<v-card-text>{{commit.message}}</v-card-text>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
-import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
 	name: 'home',
+	computed: {
+		commits() {
+			return this.repos
+				.map(repo =>
+					repo.commits.map(({ date, ...commit }) => ({
+						...commit,
+						date: new Date(date),
+						repoName: repo.name
+					}))
+				) // Get all commits
+				.flat() // Flatten structure
+				.sort((a, b) => {
+					// Sort by date descending
+					const aDate = new Date(a.date)
+					const bDate = new Date(b.date)
+					return aDate < bDate ? 1 : 0
+				})
+		},
+		...mapState(['repos', 'error', 'hasFetched'])
+	},
 	data() {
-		return {
-			commits: [],
-			componentState: 'init'
-		}
+		return {}
 	},
 	methods: {
-		async getCommits(username = 'karatekaneen') {
-			// Hard coded for now, easy to extend later
-			const resp = await axios.post('https://www.graphqlhub.com/graphql', {
-				query: `{
-						github {
-							user(username: "${username}") {
-								avatar_url
-								repos{
-									name
-									commits(limit: 5){
-										date
-										message
-									}
-								}
-							}
-						}
-					}`
-			})
-			console.log(resp.data.data.github.user.repos)
+		getIcon(i) {
+			const icons = ['pregnant_woman', 'search', 'warning']
+			return icons[i % icons.length]
+		},
+		getCommits() {
+			if (this.repos && !this.hasFetched) {
+				this.$store.dispatch('getRepos')
+			}
 		}
 	},
 	mounted() {
