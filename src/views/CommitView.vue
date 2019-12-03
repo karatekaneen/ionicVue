0b810f0832bb96b6ca9fc9242788ede2caeb2409
karatekaneen <bauhn_@hotmail.com>
2019-12-03 12:02:18 +0100

    Added demo for coordinates

--- src/views/CommitView.vue
+++ src/views/CommitView.vue
@@ -1,69 +1,42 @@
 <template>
 	<ion-content>
 		<v-container>
-			<div style="overflow-y: auto">
-				<v-card
-					style="padding: 20px; margin: 20px; "
-					v-for="(commit, index) in commits"
-					:key="index"
-				>
-					<v-card-title>
-						<v-icon x-large :color="index % 2 === 0 ? 'primary' : 'secondary'">{{
-							getIcon(index)
-						}}</v-icon>
-						{{ commit.repoName }}
-					</v-card-title>
-					<v-card-subtitle
-						>{{ commit.date.toLocaleDateString() }} -
-						{{ commit.date.toLocaleTimeString() }}</v-card-subtitle
-					>
-					<v-card-text>{{ commit.message }}</v-card-text>
-				</v-card>
+			<div>{{ keys }}</div>
+			<div>
+				{{ loc }}
 			</div>
+
+			<v-btn @click="getCurrentPosition">test</v-btn>
 		</v-container>
 	</ion-content>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { Plugins } from '@capacitor/core'
+import Coordinate from '../models/Coordinate'
+const { Geolocation } = Plugins
 export default {
 	name: 'home',
-	computed: {
-		commits() {
-			return this.repos
-				.map(repo =>
-					repo.commits.map(({ date, ...commit }) => ({
-						...commit,
-						date: new Date(date),
-						repoName: repo.name
-					}))
-				) // Get all commits
-				.flat() // Flatten structure
-				.sort((a, b) => {
-					// Sort by date descending
-					const aDate = new Date(a.date)
-					const bDate = new Date(b.date)
-					return aDate < bDate ? 1 : 0
-				})
-		},
-		...mapState(['repos', 'error', 'hasFetched'])
-	},
+	computed: {},
 	data() {
-		return {}
-	},
-	methods: {
-		getIcon(i) {
-			const icons = ['pregnant_woman', 'search', 'warning']
-			return icons[i % icons.length]
-		},
-		getCommits() {
-			if (this.repos && !this.hasFetched) {
-				this.$store.dispatch('getRepos')
-			}
+		return {
+			keys: null,
+			geo: null,
+			loc: 'init',
+			wait: null
 		}
 	},
-	mounted() {
-		this.getCommits()
-	}
+	methods: {
+		async getCurrentPosition() {
+			console.log(Geolocation)
+			const result = await Geolocation.getCurrentPosition()
+			this.keys = Object.keys(result)
+			const c = new Coordinate(result)
+			const d = c.calculateDistance({ latitude: 58.28261, longitude: 12.293189 })
+
+			this.loc = { c, d }
+		}
+	},
+	mounted() {}
 }
 </script>
