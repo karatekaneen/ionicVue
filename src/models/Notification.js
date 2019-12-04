95ec0dd57f5fb91f70c04f6965f411e2c3da1d42
karatekaneen <bauhn_@hotmail.com>
2019-12-04 11:18:54 +0100

    Added Notification class

---
+++ src/models/Notification.js
@@ -0,0 +1,76 @@
+/**
+ * Class to be used for local notifications natively.
+ */
+class Notification {
+	/**
+	 * Creates instance of a Notification.
+	 * @param {Object} params
+	 * @param {number} params.id Id of the notification
+	 * @param {String} params.title Title to be desplayed in the notification
+	 * @param {String} params.body Body for the notification
+	 * @param {String|number|Object} params.schedule Configuration for when the notification should be triggered. See `generateScheduleObj` for more detials
+	 * @param {String} params.sound What sound that should be played. **Not implemented yet**
+	 * @param {String} params.journey The Journey instance linked to this notification. To be used for when opening the ap from the notification
+	 * @param {String} params.smallIcon URI to the icon. **Not implemented yet**
+	 * @param {String} params.actionTypeId **Not implemented yet.**
+	 * @param {String} params.extra **Not implemented yet.**
+	 */
+	constructor({
+		title,
+		body = 'You are about to arrive at your destination',
+		schedule = 'now',
+		id = Math.floor(Math.random() * 100),
+		sound = null,
+		journey = null,
+		actionTypeId = null,
+		smallIcon = null, // TODO Add default icon
+		extra = null
+	} = {}) {
+		// TODO Make implementation to throw if journey is undefined
+		// if (!journey || !(journey instanceof Journey)) {
+		// 	throw new Error('Journey is required')
+		// }
+
+		this.id = id
+		this.title = title || this.getRandomTitle()
+		this.body = body
+		this.schedule = this.generateScheduleObj(schedule)
+		this.smallIcon = smallIcon
+		this.sound = sound
+		this.attachments = [journey]
+		this.actionTypeId = actionTypeId
+		this.extra = extra
+	}
+
+	/**
+	 * Returns random title if none is provided to the constructor
+	 * @returns {String} Random title
+	 */
+	getRandomTitle() {
+		const titles = [
+			`Rise and shine!`,
+			`Wakey wakey!`,
+			`Time to wake up`,
+			`Ready up!`,
+			`Adventures ahead`,
+			`You're here!`
+		]
+
+		const index = Math.round((Math.random() / 2) * 10) // Random int between 0 and 5
+		return titles[index]
+	}
+
+	/**
+	 * Generates the object that triggers the notification.
+	 * @param {String|number|Object} schedule When to send the notification. If it is a string with `now` it starts in 5 seconds, if it is a number it starts in the number of seconds. If it is an object it is considered to know what is required and is return as it is.
+	 * @returns {Object} Schedule object for the notification
+	 */
+	generateScheduleObj(schedule = 'now') {
+		if (schedule === 'now') return { at: new Date(Date.now() + 1000 * 5) }
+		else if (typeof schedule === 'number') return { at: new Date(Date.now() + 1000 * schedule) }
+		else if (typeof schedule === 'object' && schedule !== null) return schedule
+		else throw new Error('Invalid schedule')
+	}
+}
+
+export default Notification
---
+++ tests/unit/models/Notification.spec.js
@@ -0,0 +1,91 @@
+import Notification from '../../../src/models/Notification'
+
+it('has a working constructor', () => {
+	const n = new Notification()
+	expect(n instanceof Notification).toBe(true)
+})
+
+it('uses default values', () => {
+	const n = new Notification({})
+
+	expect(n.body).toBe('You are about to arrive at your destination')
+	expect(n.sound).toBe(null)
+	expect(n.actionTypeId).toBe(null)
+	expect(n.extra).toBe(null)
+	expect(n.schedule.at instanceof Date).toBe(true)
+	expect(typeof n.id).toBe('number')
+})
+
+it('Can mix both default and provided values', () => {
+	const n = new Notification({ title: 'pooop' })
+
+	expect(n.title).toBe('pooop')
+
+	expect(n.body).toBe('You are about to arrive at your destination')
+	expect(n.sound).toBe(null)
+	expect(n.actionTypeId).toBe(null)
+	expect(n.extra).toBe(null)
+	expect(n.schedule.at instanceof Date).toBe(true)
+	expect(typeof n.id).toBe('number')
+})
+
+it('generates random title if none is provided', () => {
+	const titles = [
+		`Rise and shine!`,
+		`Wakey wakey!`,
+		`Time to wake up`,
+		`Ready up!`,
+		`Adventures ahead`,
+		`You're here!`
+	]
+
+	const allCorrectTitles = new Array(10)
+		.fill(new Notification())
+		.every(notification => titles.includes(notification.title))
+
+	expect(allCorrectTitles).toBe(true)
+})
+
+it('Generates Schedule object from number', () => {
+	const n = new Notification()
+	const d = new Date()
+
+	const resp = n.generateScheduleObj(10).at
+	expect(resp instanceof Date).toBe(true)
+	expect(Math.round((resp - d) / 1000)).toBe(10) // Divides and rounds to make sure that it won't be flaky on slower machines
+})
+
+it('Generates Schedule object from string', () => {
+	const n = new Notification()
+
+	expect(n.generateScheduleObj('now').at instanceof Date).toBe(true)
+})
+
+it('throws when trying to generate Schedule object from invalid string', () => {
+	const n = new Notification()
+
+	try {
+		n.generateScheduleObj('invalidstring')
+	} catch (err) {
+		expect(err.message).toBe('Invalid schedule')
+	}
+})
+
+it('Generates Schedule object from object', () => {
+	const n = new Notification()
+
+	const scheduleOnMinute = {
+		// To schedule every minute. Guess hour and seconds should be valid too.
+		on: {
+			minute: new Date().getUTCMinutes() + 1
+		}
+	}
+
+	// To notify in intervals
+	const scheduleEvery = {
+		every: 'minute'
+	}
+
+	expect(n.generateScheduleObj(scheduleOnMinute).on.minute).toBe(new Date().getUTCMinutes() + 1)
+	expect(n.generateScheduleObj(scheduleEvery)).toEqual({ every: 'minute' })
+})
