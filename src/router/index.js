f6bfd604c90b453fb3e2c15e30631fc80133a6aa
karatekaneen <bauhn_@hotmail.com>
2019-12-05 00:36:51 +0100

    Started on Settings component

--- src/router/index.js
+++ src/router/index.js
@@ -11,20 +11,20 @@ const routes = [
 		component: RepoView
 	},
 	{
-		path: '/commits',
-		name: 'commits',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/CommitView.vue')
-	},
-	{
 		path: '/intransit',
 		name: 'inTransit',
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/InTransit.vue')
+	},
+	{
+		path: '/settings',
+		name: 'settings',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue')
 	}
 ]
 
---
+++ src/views/Settings.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="d-flex view">
+		<v-card class="info-card secondary-opaque settings-container">
+			<v-card-title>Settings</v-card-title>
+			<v-card-text>
+				{{threshold}}
+				Här ska det var inställningar
+				<!-- using :value & @input because v-model isn't implemented fully yet. -->
+				<v-slider
+					:value="threshold"
+					@input="threshold = $event"
+					thumb-label
+					:max="max"
+					:min="min"
+					hide-details
+					class="align-center"
+				></v-slider>
+			</v-card-text>
+			<div>
+				<v-btn fab outlined>abort</v-btn>
+				<v-btn fab outlined>Start</v-btn>
+			</div>
+		</v-card>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			min: -100,
+			max: 250,
+			threshold: 100
+		}
+	}
+}
+</script>
+
+<style scoped>
+.settings-container {
+	height: 70%;
+}
+</style>
