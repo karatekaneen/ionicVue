f3fae5af7444733b4c2b7efa0f4312b57f42dab8
karatekaneen <bauhn_@hotmail.com>
2019-12-04 18:39:42 +0100

    Added InTransit information component

---
+++ src/components/JourneyInfo.vue
@@ -0,0 +1,69 @@
+<template>
+	<v-card class="info-card">
+		<div class="progress-info">
+			<v-progress-circular
+				:rotate="270"
+				:size="200"
+				:value="progressPercent"
+				:width="40"
+				color="primary"
+			>
+				<div class="display-1 font-weight-black">{{ progressPercent }}%</div>
+			</v-progress-circular>
+		</div>
+		<div>
+			<span class="primary--text title"> {{ distanceLeft }}km </span>
+		</div>
+		<div>
+			<span class="primary--text title">
+				{{ timeElapsed }}
+			</span>
+		</div>
+		<v-btn outlined fab x-large @click="cancelJourney" class="cancel-button">
+			<v-icon>mdi-close</v-icon>
+		</v-btn>
+	</v-card>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			progressPercent: 27,
+			distanceLeft: 302,
+			timeElapsed: '01:23'
+		}
+	},
+	methods: {
+		cancelJourney() {
+			this.progressPercent++
+			this.distanceLeft--
+		}
+	}
+}
+</script>
+
+<style scoped>
+.info-card {
+	margin: auto;
+	background-color: rgba(247, 248, 247, 0.24);
+
+	padding: 25px 25px;
+	border-radius: 50px !important; /* Must use important to override vuetify's settings */
+	width: 70%;
+	text-align: center;
+}
+.progress-info {
+	padding-top: 1em;
+	padding-bottom: 1em;
+}
+.cancel-button {
+	margin-top: 3em;
+	margin-bottom: 2em;
+}
+
+.info-card-content {
+	/* color: rgba(0, 0, 0, 1); */
+	/* opacity: 1; */
+}
+</style>
---
+++ src/views/InTransit.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="d-flex view">
+		<JourneyInfo />
+		<particles-bg type="custom" :config="config" :bg="true" />
+	</div>
+</template>
+
+<script>
+import icon from '../assets/snowflake'
+import { ParticlesBg } from 'particles-bg-vue'
+import JourneyInfo from '../components/JourneyInfo'
+
+export default {
+	components: {
+		ParticlesBg,
+		JourneyInfo
+	},
+
+	data() {
+		return {
+			config: {
+				// TODO Gör om till vita snöflingor
+				num: [2, 5],
+				rps: 0.99,
+				radius: [5, 40],
+				life: [1.5, 4],
+				v: [0.5, 1.5],
+				tha: [180, 180],
+				body: icon,
+				alpha: [0.5, 0],
+				scale: [0.02, 0.04],
+				position: 'all',
+				cross: 'dead',
+				random: 10
+			}
+		}
+	}
+}
+</script>
+
+<style scoped></style>
