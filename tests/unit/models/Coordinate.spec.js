73ed8fdbcc3eaeb755753b9413b22ebebeb4594a
karatekaneen <bauhn_@hotmail.com>
2019-12-04 11:18:42 +0100

    Flattened structure

--- tests/unit/models/Coordinate.spec.js
+++ tests/unit/models/Coordinate.spec.js
@@ -1,33 +1,31 @@
 import Coordinate from '../../../src/models/Coordinate'
 
-describe('Coordinate', () => {
-	it('has a working constructor', () => {
-		const testResp = {
-			coords: { latitude: 58.12, longitude: 12.12, accuracy: 25 },
-			timestamp: Date.now()
-		}
-		const c = new Coordinate(testResp)
+it('has a working constructor', () => {
+	const testResp = {
+		coords: { latitude: 58.12, longitude: 12.12, accuracy: 25 },
+		timestamp: Date.now()
+	}
+	const c = new Coordinate(testResp)
 
-		expect(c instanceof Coordinate).toBe(true)
-		expect(c.longitude).toBe(testResp.coords.longitude)
-		expect(c.latitude).toBe(testResp.coords.latitude)
-	})
+	expect(c instanceof Coordinate).toBe(true)
+	expect(c.longitude).toBe(testResp.coords.longitude)
+	expect(c.latitude).toBe(testResp.coords.latitude)
+})
 
-	it('converts degrees to radians', () => {
-		const c = new Coordinate()
+it('converts degrees to radians', () => {
+	const c = new Coordinate()
 
-		expect(c.toRadians(89)).toBe(1.5533430342749532)
-		expect(c.toRadians(180)).toBe(3.141592653589793)
-	})
+	expect(c.toRadians(89)).toBe(1.5533430342749532)
+	expect(c.toRadians(180)).toBe(3.141592653589793)
+})
 
-	it('calculates distances between points correctly', () => {
-		const testResp = {
-			coords: { latitude: 58.12, longitude: 12.12, accuracy: 25 },
-			timestamp: Date.now()
-		}
-		const c = new Coordinate(testResp)
+it('calculates distances between points correctly', () => {
+	const testResp = {
+		coords: { latitude: 58.12, longitude: 12.12, accuracy: 25 },
+		timestamp: Date.now()
+	}
+	const c = new Coordinate(testResp)
 
-		expect(c.calculateDistance({ latitude: 58, longitude: 12 })).toBe(15096) // Verified by manual calulation
-		expect(c.calculateDistance({ latitude: 59, longitude: 13 })).toBe(110361) // Verified by manual calulation
-	})
+	expect(c.calculateDistance({ latitude: 58, longitude: 12 })).toBe(15096) // Verified by manual calulation
+	expect(c.calculateDistance({ latitude: 59, longitude: 13 })).toBe(110361) // Verified by manual calulation
 })
