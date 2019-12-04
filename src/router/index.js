152be60e95a6b52669d9e6489b03ef6047a82766
karatekaneen <bauhn_@hotmail.com>
2019-12-04 18:40:04 +0100

    Added InTransit

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ '../views/CommitView.vue')
+	},
+	{
+		path: '/intransit',
+		name: 'inTransit',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/InTransit.vue')
 	}
 ]
 
