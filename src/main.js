41fc9bb671c959c034c52efe4523dc882b5871c5
karatekaneen <bauhn_@hotmail.com>
2019-12-05 11:36:51 +0100

    Added maps

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 import Ionic from '@ionic/vue'
 import '@ionic/core/css/ionic.bundle.css'
@@ -11,6 +12,13 @@ Vue.config.ignoredElements = [/^ion-/, /^jeep-/] // added line
 
 Vue.use(Ionic)
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+	load: {
+		key: process.env.VUE_APP_GOOGLE_API_KEY,
+		libraries: 'places'
+	}
+})
 
 new Vue({
 	router,
