7b9aebc57d6f3846075faa7c0b674f83c7a19a77
karatekaneen <bauhn_@hotmail.com>
2019-12-08 13:16:34 +0100

    Added dialog for cancelling journey

--- src/components/JourneyInfo.vue
+++ src/components/JourneyInfo.vue
@@ -17,9 +17,27 @@
 		<div>
 			<span class="primary--text title">{{ progress.timeElapsed }}</span>
 		</div>
-		<v-btn outlined fab x-large @click="cancelJourney" class="cancel-button">
-			<v-icon>mdi-close</v-icon>
-		</v-btn>
+		<v-dialog v-model="dialog" width="300">
+			<template v-slot:activator="{ on }">
+				<v-btn outlined dark fab x-large v-on="on" class="cancel-button">
+					<v-icon>mdi-close</v-icon>
+				</v-btn>
+			</template>
+
+			<v-card>
+				<v-card-title class="headline grey lighten-2" primary-title>Cancel Journey</v-card-title>
+
+				<v-card-text>Do you want to cancel the journey?</v-card-text>
+
+				<v-divider></v-divider>
+
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn color="primary" rounded @click="dialog = false">No</v-btn>
+					<v-btn color="error" rounded @click="cancelJourney">Yes</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</v-card>
 </template>
 
@@ -27,8 +45,23 @@
 import { mapState } from 'vuex'
 export default {
 	computed: { ...mapState(['progress']) },
+	data() {
+		return {
+			dialog: false
+		}
+	},
 	methods: {
-		cancelJourney() {}
+		/**
+		 * Cancels the current journey and resets the state.
+		 * Pushes the user back to the destination view.
+		 * @TODO Change destination to splash-page instead
+		 * @returns {void}
+		 */
+		async cancelJourney() {
+			await this.$store.dispatch('journeyRemoval')
+			this.dialog = false
+			this.$router.push({ name: 'destination' })
+		}
 	}
 }
 </script>
